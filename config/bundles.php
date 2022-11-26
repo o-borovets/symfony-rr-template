0b2ee78e39396af769b214668ef276a8a6c4be3f
Oleksandr Borovets <oleksandr.borovets@gmail.com>
2022-11-27 00:25:31 +0200

    Moved SymfonyKernel to the system module

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Baldinof\RoadRunnerBundle\BaldinofRoadRunnerBundle::class => ['all' => true],
+    RoadRunnerTemporalSymfony\TemporalSymfonyBundle::class => ['all' => true],
 ];
--- src/System/Infrastructure/Symfony/Kernel.php
+++ src/System/Infrastructure/Symfony/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\System\Infrastructure\Symfony;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
