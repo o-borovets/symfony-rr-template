1a9fdd1c017afb7c15121ed40457859d5dd0f929
Oleksandr Borovets <oleksandr.borovets@gmail.com>
2023-06-05 00:24:49 +0300

    Updated rector to 0.17

--- rector.php
+++ rector.php
@@ -1,17 +1,16 @@
 <?php
 
+use Rector\CodeQuality\Rector\Class_\CompleteDynamicPropertiesRector;
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
 use Rector\Config\RectorConfig;
-use Rector\Core\Configuration\Option;
-use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
 return function (RectorConfig $rectorConfig) {
     $rectorConfig->parallel();
-    $rectorConfig->importNames();
+    $rectorConfig->importNames(true, false);
     $rectorConfig->importShortClasses(false);
-
-    $rectorConfig->parameters()->set(Option::APPLY_AUTO_IMPORT_NAMES_ON_CHANGED_FILES_ONLY, true);
 
     $rectorConfig->cacheDirectory(__DIR__ . '/var/rector');
 
@@ -25,10 +24,18 @@ return function (RectorConfig $rectorConfig) {
     $rectorConfig->sets([
         SetList::TYPE_DECLARATION,
         SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::DEAD_CODE,
         SetList::EARLY_RETURN,
-        LevelSetList::UP_TO_PHP_81,
+        LevelSetList::UP_TO_PHP_82,
     ]);
 
+    $rectorConfig->rule(DeclareStrictTypesRector::class);
+
     $rectorConfig->skip([
+        CompleteDynamicPropertiesRector::class => [
+            __DIR__ . 'src/System/Infrastructure/HealthCheck/services.php:1',
+        ],
+        EncapsedStringsToSprintfRector::class,
     ]);
 };
