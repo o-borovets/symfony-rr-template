68c89542cef0552bdd37f3ce66a29bd3a7724e1a
Oleksandr Borovets <oleksandr.borovets@gmail.com>
2023-06-05 03:13:47 +0300

    Added ability to register services and routers within packages

--- src/System/Infrastructure/Symfony/Kernel.php
+++ src/System/Infrastructure/Symfony/Kernel.php
@@ -5,9 +5,31 @@ declare(strict_types=1);
 namespace App\System\Infrastructure\Symfony;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
-    use MicroKernelTrait;
+    use MicroKernelTrait {
+        configureContainer as baseConfigureContainer;
+        configureRoutes as baseConfigureRoutes;
+    }
+
+    protected function configureRoutes(RoutingConfigurator $routes): void
+    {
+        $this->baseConfigureRoutes($routes);
+
+        $routes->import($this->getProjectDir() . '/src/**/{routes}.{yaml,php}', 'glob');
+    }
+
+    protected function configureContainer(ContainerConfigurator $container, LoaderInterface $loader, ContainerBuilder $builder): void
+    {
+        $this->baseConfigureContainer($container, $loader, $builder);
+
+        $loader->load($this->getProjectDir() . '/src/**/{services}.{yaml,php}', 'glob');
+        $loader->load($this->getProjectDir() . "/src/**/{services_{$this->environment}}.{yaml,php}", 'glob');
+    }
 }
